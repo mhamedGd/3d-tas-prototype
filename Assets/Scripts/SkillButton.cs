c6696632e0abcd55c526f4d97a5d5da8fb3e1e2a
mhamedgd <mhamed.m14@gmail.com>
2025-03-08 15:08:24 +0300

    Init Skill Descrip

---
+++ Assets/Scripts/SkillButton.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SkillButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] string title;
+    [SerializeField, TextArea] string description;
+
+    TextMeshProUGUI _description;
+    private void Start()
+    {
+        _description = GameObject.FindGameObjectWithTag("Description").GetComponent<TextMeshProUGUI>();
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _description.text = description;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
